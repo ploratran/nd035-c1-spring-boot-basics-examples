5332e94cc52fbf2862839a0c7e00e01e0634bc92
Plora Tran <ploratran@gmail.com>
2020-11-22 16:11:45 -0800

    display an array of strings

--- udacity-jwdnd-c1-l3-mvc-and-you-2-master/src/main/java/com/example/mvc_basics/HomeController.java
+++ udacity-jwdnd-c1-l3-mvc-and-you-2-master/src/main/java/com/example/mvc_basics/HomeController.java
@@ -13,7 +13,7 @@ public class HomeController {
     // add "model" parameter to Controller's method:
     public String getHomePage(Model model) {
         // add attribute "welcomeMessage" to a map-like object model:
-        model.addAttribute("welcomeMessage", "Current time is: " + Instant.now().toString());
+        model.addAttribute("greetings", new String[]{ "Hi", "Hello", "Goodbye" });
         return "home";
     }
 
