b3b4991f2cc43f3ad91f567db3baf81caecd0e3b
Plora Tran <ploratran@gmail.com>
2020-11-27 21:25:21 -0800

    implement chat.html + ChatForm and ChatMessage classes

--- review-exercise/src/main/java/com/example/reviewexercise/model/ChatForm.java
+++ review-exercise/src/main/java/com/example/reviewexercise/model/ChatForm.java
@@ -1,4 +1,32 @@
 package com.example.reviewexercise.model;
 
+// data type class for form fields
 public class ChatForm {
+    private String username;
+    private String text;
+    private String messageType;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(String messageType) {
+        this.messageType = messageType;
+    }
 }
