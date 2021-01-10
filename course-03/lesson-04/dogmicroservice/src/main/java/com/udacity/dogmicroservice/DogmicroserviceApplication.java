74258347f7d07549b9005755b4eee1bf36f2dab4
Plora Tran <ploratran@gmail.com>
2021-01-10 15:21:51 -0800

    register Dogmicroservice as a Eureka client in application.properties

--- course-03/lesson-04/dogmicroservice/src/main/java/com/udacity/dogmicroservice/DogmicroserviceApplication.java
+++ course-03/lesson-04/dogmicroservice/src/main/java/com/udacity/dogmicroservice/DogmicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.udacity.dogmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient // this is optional due to spring-cloud-starter-netflix-eureka-client dependencies
 public class DogmicroserviceApplication {
 
 	public static void main(String[] args) {
